d2f12944efff40621c2843f84551f5cee3891512
Sanchit <sanchit0496@gmail.com>
2021-08-31 02:23:15 -0700

    fetching the continent names

--- vanilla-project/script.js
+++ vanilla-project/script.js
@@ -1,3 +1,35 @@
-fetch('https://countries.trevorblades.com/')
-.then(x => x.json())
-.then(y => console.log(y));
+let continentNames = document.getElementById('continent-names')
+let countryNames = document.getElementById('country-names')
+
+fetch('https://countries.trevorblades.com/', {
+    method: 'POST',
+    headers: {
+        "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+     query: `
+     query{
+        continents{
+          code
+          name
+        }
+      }
+     `
+    })
+})
+
+.then(data => data.json())
+.then(results => {
+    results.data.continents.forEach((cName) => {
+        let option = document.createElement('option')
+        option.value = cName.code
+        option.innerText = cName.name
+        continentNames.append(option)
+    })
+});
+
+continentNames.addEventListener('change', getCountries(e.target.value))
+
+function getCountries(continentCode){
+
+}
